c1890d5833bce3f9718d5e2ee50babc59a577ae0
Jonathan Giorgi Silveira <jonathan.silveira-ext@ab-inbev.com>
2024-04-11 21:33:51 -0300

    Styling elements and filter

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const onButtonClicked = () => {
 }
 
 const onSearchButtonClicked = async () => {
-    let startDate = '2024-04-01';//document.getElementById('data_inicio').value;
-    let endDate = '2024-04-26';//document.getElementById('data_fim').value;
+    let startDate = document.getElementById('filter-start-date').value;
+    let endDate = document.getElementById('filter-end-date').value;
 
     let url = `${baseUrl}/agenda_servicos?data_inicio=${startDate}&data_fim=${endDate}`;
     fetch(url, {method: 'get'})
@@ -18,21 +18,22 @@ const onSearchButtonClicked = async () => {
 }
 
 const updateScheduleList = (schedules) => {
-    document.getElementById('scheduleList').innerHTML = '';
+    document.getElementById('schedule-list').innerHTML = '';
 
     schedules.forEach(item => addSchedule(item));
 }
 
 const addSchedule = (schedule) => {
-    var itemElement = document.createElement("div");
+    var itemElement = document.createElement("div")
     itemElement.id = 'scheduleItem'
-    let itemIdElement = createItemIdElement(schedule.id);
-    var contentElement = document.createTextNode(`Cliente ${schedule.nome_cliente} irá trazer ${schedule.nome_pet} para fazer ${schedule.servico_titulo}`);
+    itemElement.className = 'schedule-item'
+    let itemIdElement = createItemIdElement(schedule.id)
+    var contentElement = document.createTextNode(`Cliente ${schedule.nome_cliente} irá trazer ${schedule.nome_pet} para fazer ${schedule.servico_titulo}`)
     
     itemElement.appendChild(itemIdElement)
     itemElement.appendChild(contentElement)
 
-    document.getElementById('scheduleList').appendChild(itemElement)
+    document.getElementById('schedule-list').appendChild(itemElement)
 }
 
 const createItemIdElement = (value) => {
@@ -41,3 +42,13 @@ const createItemIdElement = (value) => {
     element.value = value;
     return element;
 }
+
+const initDateFilters = () => {
+    let today = new Date()
+    var startDateElement = document.getElementById('filter-start-date')
+    var endDateElement = document.getElementById('filter-end-date')
+    startDateElement.valueAsDate = today
+    endDateElement.valueAsDate = today
+}
+
+initDateFilters()
